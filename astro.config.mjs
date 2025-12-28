2451ee1408cb3eed7c37ab0fa05d8ae569c59b88
solankydev <deependra@solanky.dev>
2025-12-28 18:43:38 +0530

    feat: Add sitemap for better search engine indexing (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://openza.github.io',
   build: {
     assets: '_assets'
-  }
+  },
+  integrations: [sitemap()]
 });
